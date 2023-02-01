675d62174e34f8eedb51bd00d735dd8322da7341
Gabriel Magaton <gabriel.magaton@bv.com.br>
2023-02-01 15:21:22 -0300

    Excel artigos / Melhoria emulador drone

--- src/droneEmulation.py
+++ src/droneEmulation.py
@@ -12,35 +12,37 @@ def topology(args):
     "Create a network."
     net = Mininet_wifi()
 
+    c0 = net.addController()
+
     info("*** Creating nodes\n")
 
     ap1 = net.addAccessPoint('ap-1', ssid='new-ssid', mode='g', channel='1',
-                             failMode="standalone", position='50,50,0', range=30)
+                             failMode="standalone", position='50,50,0', range=90)
 
     var_name = "drone-{}"
-    var_mac = "00:00:00:00:00:{:02d}"
-    var_ip = "10.0.0.{}/8"
 
-    for x in range(2, 23):
-        net.addStation(var_name.format(x), mac=var_mac.format(x), ip=var_ip.format(x),
-                   min_x=0, max_x=80, min_y=0, max_y=80, min_v=5, max_v=80, range=20)        
+    for x in range(1, 21):
+        net.addStation(var_name.format(x), range=40)        
 
-    info("*** Configuring propagation model\n")
+    #info("*** Configuring propagation model\n")
     net.setPropagationModel(model="logDistance", exp=5)
 
     info("*** Configuring nodes\n")
     net.configureNodes()
 
-    for x in range(2, 23):
-        net.addLink(var_name.format(x), ap1)
+    for x in range(1, 21):
+        net.addLink(ap1, var_name.format(x))
 
-    if '-p' not in args:
-        net.plotGraph(max_x=200, max_y=200)
+    net.plotGraph(max_x=200, max_y=200)
 
-    net.setMobilityModel(time=0, model='RandomDirection', max_x=100, max_y=100, seed=2)
+    net.setMobilityModel(time=0, model='RandomDirection', max_x=100, max_y=100, seed=20)
 
     info("*** Starting network\n")
     net.build()
+
+    net.controllers[0].start()
+    net.get('ap-1').start([c0])
+
     ap1.start([])
 
     info("*** Running CLI\n")
